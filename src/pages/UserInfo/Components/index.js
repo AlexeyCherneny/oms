95fdda5fc8a01939118b9cb4b0e97e4addfaf6f6
Holenvik <mandarinkajok@gmail.com>
2020-01-22 12:01:18 +0300

    added field for user info and style for panel

--- src/pages/UserInfo/Components/index.js
+++ src/pages/UserInfo/Components/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Row, Col, Avatar } from "antd";
+import * as styles from "./styles.module.scss";
 
 // import UserInfoForm from "../../../Components/Forms/UserInfo";
 
@@ -9,35 +10,52 @@ const UserInfo = props => {
   if (!user) return null;
 
   return (
-    <div>
-      <Row style={{ marginBottom: 15 }}>
+    <>
+      <Row className={styles.userPhotoNameWrapper}>
         <Col span={6}>
-          <Avatar
-            src={user.photo}
-            size={64}
-          />
+          <Avatar src={user.photo} size={64} />
         </Col>
         <Col span={18}>
-          <h2>{user.first_name + " " + user.last_name}</h2>
+          <h1 className={styles.userName}>
+            {user.first_name + " " + user.last_name + " " + user.middle_name}
+          </h1>
         </Col>
       </Row>
-      <Row>
-        <Col span={6}>
-          <h3>Телефон:</h3>
-        </Col>
-        <Col span={18}>
-          <h3>{user.phone}</h3>
-        </Col>
+      <Row className={styles.addInfoWrapper}>
+        <Row>
+          <Col span={18}>
+            <h2>Телефон:</h2>
+          </Col>
+          <Col span={18}>
+            <p className={styles.addInfo}>{user.phone}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={18}>
+            <h2>Должность:</h2>
+          </Col>
+          <Col span={18}>
+            <p className={styles.addInfo}>{user.position}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={18}>
+            <h2>Дата рождения:</h2>
+          </Col>
+          <Col span={18}>
+            <p className={styles.addInfo}>{user.birthday}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={18}>
+            <h2>Email:</h2>
+          </Col>
+          <Col span={18}>
+            <p className={styles.addInfo}>{user.email}</p>
+          </Col>
+        </Row>
       </Row>
-      <Row>
-        <Col span={6}>
-          <h3>Должность:</h3>
-        </Col>
-        <Col span={18}>
-          <h3>{user.position}</h3>
-        </Col>
-      </Row>
-    </div>
+    </>
   );
 };
 
