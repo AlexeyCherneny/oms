597b3e60f2731d850ae83b2b9615429a253131ec
AlexeyCherneny <a.cherneny@gmail.com>
2020-02-06 18:42:05 +0300

    Fix api endpoint for development

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,16 @@ const proxy = require("http-proxy-middleware");
 
 const stubs =
   process.argv.includes("--stubs") || process.argv.includes("--stubs=true");
-const api = stubs ? "http://localhost:3100/" : process.env.API_URL;
+
+let api = process.env.PRODUCTION_API_URL;
+
+if (process.env.NODE_ENV === "development") {
+  if (stubs) {
+    api = process.env.STUB_API_URL;
+  } else {
+    api = process.env.DEVELOPMENT_API_URL;
+  }
+}
 
 module.exports = function(app) {
   app.use(
