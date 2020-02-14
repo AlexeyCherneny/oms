b56dacc5cb391b5944e4de9ef41dd8a81f2d8ba1
AlexeyCherneny <a.cherneny@gmail.com>
2020-02-14 14:13:38 +0300

    Roll back unecessary changed files

--- src/Components/Spinner/index.js
+++ src/Components/Spinner/index.js
@@ -17,8 +17,8 @@ export default ({
   const [ isSpinnig, setSpinnig ] = React.useState(false);
   React.useEffect(() => {
     if (delay && isSpinnig && !spinning) {
-      const timerUuid = setTimeout(() => setSpinnig(spinning), delay);
-      return () => clearTimeout(timerUuid);
+      const timerId = setTimeout(() => setSpinnig(spinning), delay);
+      return () => clearTimeout(timerId);
     } else {
       setSpinnig(spinning);
     }
--- src/settings/index.js
+++ src/settings/index.js
@@ -2,10 +2,10 @@ export const FIREBASE_SETTINGS = {
   apiKey: "AIzaSyDxVk08YEPeTujPwteFZr5GtUEMueVoSo4",
   authDomain: "stacklevel-oms.firebaseapp.com",
   databaseURL: "https://stacklevel-oms.firebaseio.com",
-  projectUuid: "stacklevel-oms",
+  projectId: "stacklevel-oms",
   storageBucket: "stacklevel-oms.appspot.com",
-  messagingSenderUuid: "635125630338",
-  appUuid: "1:635125630338:web:cfdbecd23b2c7f16c3bb9b"
+  messagingSenderId: "635125630338",
+  appId: "1:635125630338:web:cfdbecd23b2c7f16c3bb9b"
 };
 
 export const VAPID_KEY =
